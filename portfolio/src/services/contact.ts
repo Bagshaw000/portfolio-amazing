bc9e9c5d755256bd0c0c297f5322e3584a6edefa
Omieibi Bagshaw <62633996+Bagshaw000@users.noreply.github.com>
2024-12-17 20:39:24 +0100

    Push recent changes

--- portfolio/src/services/contact.ts
+++ portfolio/src/services/contact.ts
@@ -5,7 +5,10 @@ import { IClientEnquiry } from "./types";
 export async function clientEnquiry(data: IClientEnquiry) {
   try {
     console.log(data);
-    await axios.post("http://localhost:8080/email", data);
+    await axios.post(
+      "https://portfolio-amazing-backend.onrender.com:10000/email",
+      data
+    );
   } catch (error) {
     console.error("Error sending message:", error);
     throw error;
